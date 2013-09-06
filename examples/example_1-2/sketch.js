fb88d92c702305472e480f9f868aaf332a13d71c
lauren mccarthy <laurmccarthy@gmail.com>
2013-09-06 17:02:01 -0400

    updating examples and assignments page

---
+++ examples/example_1-2/sketch.js
@@ -0,0 +1,11 @@
+var setup = function() {
+  createGraphics(200, 200);
+};
+
+var draw = function() {
+  background(250, 250, 50);
+  fill(255, 0, 0);
+  rect(0, 0, 50, 50);
+  fill(0, 255, 0);
+  ellipse(100, 100, 100, 50);
+};
