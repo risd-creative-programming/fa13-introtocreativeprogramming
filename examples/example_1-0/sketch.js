0e91da00b3f79388968f188c6d9c1b1f5cb722b9
lauren mccarthy <laurmccarthy@gmail.com>
2013-09-06 17:12:18 -0400

    updating examples

--- examples/example_1-0/sketch.js
+++ examples/example_1-0/sketch.js
@@ -1,11 +1,14 @@
 var setup = function() {
-  createGraphics(200, 200);
+  createGraphics(600, 400);
 };
 
 var draw = function() {
   background(250, 250, 50);
-  fill(255, 0, 0);
-  rect(0, 0, 50, 50);
-  fill(0, 255, 0);
-  ellipse(100, 100, 100, 50);
+
+	strokeWeight(10);
+	line(100, 100, 100, 300);
+	line(120, 100, 120, 300);
+	line(140, 100, 140, 300);
+	line(160, 100, 160, 300);
+	
 };
--- examples/example_1-1/sketch.js
+++ examples/example_1-1/sketch.js
@@ -1,11 +1,32 @@
+
+// A for loop has 3 parts. 
+// The init part declares a new value to be used just within the loop, and assigns it a value. i is used a lot, but it can be anything. 
+// The test part evaluates the value of the variable. As long as the value is true, the statements part is evaluated. 
+// The update part changes the variable.
+
+// for (init; test; update) {
+//  statements
+// }
+
+
 var setup = function() {
-  createGraphics(400, 400);
+  createGraphics(600, 400);
 };
 
 var draw = function() {
   background(250, 250, 50);
-  fill(255, 0, 0);
-  rect(0, 0, 50, 50);
-  fill(0, 255, 0);
-  ellipse(100, 100, 100, 50);
+
+	strokeWeight(10);
+
+	// line(100, 100, 100, 300);
+	// line(120, 100, 120, 300);
+	// line(140, 100, 140, 300);
+	// line(160, 100, 160, 300);
+
+	for (var i=0; i<4; i++) {
+		line(100 + 20*i, 100, 100 + 20*i, 300);
+	}
+
+	// Try changing 4 to other numbers... 8, 12, 100?!
+	
 };
