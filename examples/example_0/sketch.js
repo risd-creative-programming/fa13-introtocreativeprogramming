885a9ef192ff64d0d5eadd57d2218b3ebd0c3cc1
Evelyn Eastmond <evelyn@bocoup.com>
2013-09-10 19:25:13 -0400

    making example 0 bigger

--- examples/example_0/sketch.js
+++ examples/example_0/sketch.js
@@ -1,11 +1,20 @@
 var setup = function() {
-  createGraphics(200, 200);
+
+  createGraphics(600, 400);
+  
 };
 
 var draw = function() {
-  background(250, 250, 50);
-  fill(255, 0, 0);
-  rect(0, 0, 50, 50);
-  fill(0, 255, 0);
-  ellipse(100, 100, 100, 50);
+
+  // background
+  background(250,250,100);
+
+  // ellipse
+  fill(250,200,200);
+  ellipse(100,100,100,100);
+
+  //rectangle
+  fill(200,200,250);
+  rect(100,400,50,50);
+
 };
