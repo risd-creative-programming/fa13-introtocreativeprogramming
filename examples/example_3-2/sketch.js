4dc3e3b5b1dac7a9682cef66eee9ae990ab05bf9
Evelyn Eastmond <evelyn@bocoup.com>
2013-09-25 12:49:46 -0400

    or example

--- examples/example_3-2/sketch.js
+++ examples/example_3-2/sketch.js
@@ -1,5 +1,5 @@
 
-// Check location with 4 quadrants
+// Check location with OR
 
 var setup = function() {
 
@@ -13,19 +13,8 @@ var draw = function() {
 
 	background(204);
 
-	if ((mouseX < 300) && (mouseY < 300)) { // upper left
-		rect(0, 0, 300, 300);
-	}
-
-	else if ((mouseX < 300) && (mouseY >= 300)) { // lower left
-		rect(0, 300, 300, 300);
-	}
-
-	else if ((mouseX >= 300) && (mouseY < 300)) { // upper right
-		rect(300, 0, 300, 300);
-
-	} else { // lower right
-		rect(300, 300, 300, 300);
+	if ((mouseX < 300) || (mouseY < 300)) { // either or
+		ellipse(300,300,50,50);
 	}
 
 };
