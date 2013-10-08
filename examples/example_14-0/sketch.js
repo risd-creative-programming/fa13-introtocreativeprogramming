92f117554179e9a8bdc80ad3e1106ec495cfc871
Evelyn Eastmond <evhan55@gmail.com>
2013-10-07 21:49:48 -0400

    fixing example 0

--- examples/example_14-0/sketch.js
+++ examples/example_14-0/sketch.js
@@ -3,22 +3,19 @@
 var red = 0;
 
 var setup = function() {
-  createGraphics(800, 1000); // set to fit a standard-ish smartphone
+  createGraphics(900, 1200); // set to fit a standard-ish smartphone
   noStroke();
   fill(255, 255, 255);
 };
 
 var draw = function() {
-  background(red, 255, 255);
+  background(red, 0, 255);
   rect(300, 200, 200, 600); // simulated button
-};
 
-var touchStart = function() {
-  // touchX has the touch's x coordinate
-  // touchY has the touch's y coordinate
   if((touchX > 300) && (touchX < 500) && (touchY > 200) && (touchY < 800)){ // if touch inside rectangle
     red = 255;
   } else {
     red = 0;
   }
-}
+  
+};
