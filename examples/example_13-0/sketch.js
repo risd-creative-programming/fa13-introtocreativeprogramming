509d3f5166650e9781a35343de06c169ede6315b
Evelyn Eastmond <evhan55@gmail.com>
2013-10-09 00:39:33 -0400

    starting to add pixel examples

--- examples/example_13-0/sketch.js
+++ examples/example_13-0/sketch.js
@@ -1,15 +1,14 @@
-
 var setup = function() {
-  createGraphics(600, 600); // set to fit a standard-ish smartphone
+  createGraphics(600, 600); 
   noStroke();
   noLoop();
 }
 
 var draw = function() {
-  var myImage = loadImage("jennifer.png"); // this image came from my Instagram feed :X
+  var myImage = loadImage("jennifer.png"); // this image came from Evelyn's Instagram feed :X
   image(myImage, 0, 0, 600, 600);
-  var c = get(100, 100);
-  print(c);
+  var c = get(242, 162); // from the pink area
+  //var c = get(393,273); // from the body suit
   fill(c);
   rect(100, 100, 300, 300);
 };
