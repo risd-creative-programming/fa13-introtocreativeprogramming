d38c5cdc4386adbc8d93871a3b041a1849c72b43
Evelyn Eastmond <evhan55@gmail.com>
2013-10-29 00:27:02 -0400

    testing external library

--- examples/example_18-0/sketch.js
+++ examples/example_18-0/sketch.js
@@ -3,68 +3,17 @@
 //////////////
 
 var setup = function() {
+  var engine = new RainyDay({
+    element: 'rain',        // ID of image element
+    blur: 10,               // Defines blur due to rain effect
+    opacity: 1,             // Opacity of rain drops
+    fps: 30                 // Frame rate per second
+  });
+  engine.rain([ [3, 3, 0.88], [5, 5, 0.9], [6, 2, 1] ], 100);
 };
 
 var draw = function() {
 
 };
 
-//////////////////
-// THREE.JS CODE
-//////////////////
 
-
-var camera, scene, renderer;
-var mesh;
-
-init();
-animate();
-
-function init() {
-
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild( renderer.domElement );
-
-  //
-
-  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
-  camera.position.z = 400;
-
-  scene = new THREE.Scene();
-
-  var geometry = new THREE.CubeGeometry( 200, 200, 200 );
-
-  var texture = THREE.ImageUtils.loadTexture( 'textures/crate.gif' );
-  texture.anisotropy = renderer.getMaxAnisotropy();
-
-  var material = new THREE.MeshBasicMaterial( { map: texture } );
-
-  mesh = new THREE.Mesh( geometry, material );
-  scene.add( mesh );
-
-  //
-
-  window.addEventListener( 'resize', onWindowResize, false );
-
-}
-
-function onWindowResize() {
-
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize( window.innerWidth, window.innerHeight );
-
-}
-
-function animate() {
-
-  requestAnimationFrame( animate );
-
-  mesh.rotation.x += 0.005;
-  mesh.rotation.y += 0.01;
-
-  renderer.render( scene, camera );
-
-}
