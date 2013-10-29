b9b7767fefa5b7d52bef04dfea263e645e648577
Evelyn Eastmond <evhan55@gmail.com>
2013-10-29 00:33:11 -0400

    testing canvas + library

--- examples/example_18-0/sketch.js
+++ examples/example_18-0/sketch.js
@@ -1,8 +1,7 @@
-//////////////
-// SKETCH
-//////////////
-
 var setup = function() {
+  ///////////////////
+  // Rainyday example
+  ///////////////////
   var engine = new RainyDay({
     element: 'rain',        // ID of image element
     blur: 10,               // Defines blur due to rain effect
@@ -10,10 +9,15 @@ var setup = function() {
     fps: 30                 // Frame rate per second
   });
   engine.rain([ [3, 3, 0.88], [5, 5, 0.9], [6, 2, 1] ], 100);
+
+  ////////////////
+  // Normal sketch
+  ////////////////
+  var c = createGraphics(900, 900);
 };
 
 var draw = function() {
-
+  background(255, 200, 200);
 };
 
 
